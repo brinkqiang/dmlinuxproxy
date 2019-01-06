31e28b4e7d23c5cbf92ffbf7e1a7488b3c1b1c16
brinkqiang <brink.qiang@gmail.com>
2019-01-06 21:54:16 +0800

    add

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 # iptables-restore > /etc/sysconfig/iptables
 # vim /etc/sysconfig/iptables
 
-./shadowiptables.sh start
+bash shadowiptables.sh start
 # Start the shadowsocks-redir
 # ss-server -s 0.0.0.0 -p 52020 -k 91idol.com -u -m chacha20 &
 ss-redir -u -s 91idol.com -p 52020 -l 51080 -m chacha20 -k 91idol.com
-./shadowiptables.sh stop
+bash shadowiptables.sh stop
