8d74f22ec2d2d4f3867b8292b1a57aba78cc4949
brinkqiang <brink.qiang@gmail.com>
2019-01-06 18:46:06 +0800

    add

--- install.sh
+++ install.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
+# vim /etc/sysconfig/iptables-config
+# iptables-save > /etc/sysconfig/iptables
+# iptables-restore > /etc/sysconfig/iptables
+# vim /etc/sysconfig/iptables
 
-ss-local -s qq.com -p server_port -l local_port -m chacha20 -k passwd
+# Create new chain
+iptables -t nat -N SHADOWSOCKS
+iptables -t mangle -N SHADOWSOCKS
+
+# Ignore your shadowsocks server's addresses
+# It's very IMPORTANT, just be careful.
+iptables -t nat -A SHADOWSOCKS -d 123.123.123.123 -j RETURN
+
+# Ignore LANs and any other addresses you'd like to bypass the proxy
+# See Wikipedia and RFC5735 for full list of reserved networks.
+# See ashi009/bestroutetb for a highly optimized CHN route list.
+iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+
+# Anything else should be redirected to shadowsocks's local port
+iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 12345
+
+# Add any UDP rules
+ip route add local default dev lo table 100
+ip rule add fwmark 1 lookup 100
+iptables -t mangle -A SHADOWSOCKS -p udp --dport 53 -j TPROXY --on-port 51080 --tproxy-mark 0x01/0x01
+
+# Apply the rules
+iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
+iptables -t mangle -A PREROUTING -j SHADOWSOCKS
+
+# Start the shadowsocks-redir
+# ss-server -s 0.0.0.0 -p 52020 -k 91idol.com -u -m chacha20 &
+ss-redir -u -s 91idol.com -p 52020 -l 51080 -m chacha20 -k 91idol.com
