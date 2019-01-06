dc2b22852b87bf1918caa164df3859f35480d111
brinkqiang <brink.qiang@gmail.com>
2019-01-06 22:27:12 +0800

    add

--- shadowiptables.sh
+++ shadowiptables.sh
@@ -1,65 +1,67 @@
 #!/bin/bash
-#自动翻墙脚本，配合shadowsocks-libev的ss-redir使用。需要ipset
-chnroute_file=~/.chnroute
+# china_ip_list.txt from https://github.com/17mon/china_ip_list
+
+chnroute_file=~/china_ip_list/china_ip_list.txt
 ignore_ips=(
-	45.32.50.160
-	45.127.93.239
-	103.214.68.175
 	0.0.0.0/8
 	10.0.0.0/8
+	17.0.0.0/8
+	100.64.0.0/10
 	127.0.0.0/8
 	169.254.0.0/16
 	172.16.0.0/12
 	192.168.0.0/16
 	224.0.0.0/4
 	240.0.0.0/4
-	45.125.195.43
-	46.8.255.210
-	23.88.239.57
-	103.217.253.73
-	198.98.125.36
-	47.75.48.245
+	203.205.128.0/18
 )
+src_addr=47.75.48.245/24
 local_port=51080
 
 update(){
-  curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > $chnroute_file
-  echo "127.0.0.1/24" >> $chnroute_file
+	if pushd ~/china_ip_list; then git pull; else git clone https://github.com/17mon/china_ip_list ~/china_ip_list; fi
+        popd
 }
 
 start_rule(){
 	check_for_update
-	sudo iptables -t nat -C OUTPUT -p tcp -j SHADOWTABLES >& /dev/null \
-		|| sudo iptables -t nat -A OUTPUT -p tcp -j SHADOWTABLES
+	iptables -t nat -C OUTPUT -p tcp -s $src_addr -j SHADOWTABLES >& /dev/null \
+		|| iptables -t nat -A OUTPUT -p tcp -s $src_addr -j SHADOWTABLES
 }
 
 apply_redir(){
-	sudo iptables -t nat -C PREROUTING -p tcp -j SHADOWTABLES >& /dev/null \
-		|| sudo iptables -t nat -A PREROUTING -p tcp -j SHADOWTABLES
+	iptables -t nat -C PREROUTING -p tcp -s $src_addr -j SHADOWTABLES >& /dev/null \
+		|| iptables -t nat -A PREROUTING -p tcp -s $src_addr -j SHADOWTABLES
 }
 
 stop_rule(){
-	sudo iptables -t nat -C OUTPUT -p tcp -j SHADOWTABLES >& /dev/null \
-		&& sudo iptables -t nat -D OUTPUT -p tcp -j SHADOWTABLES
+	iptables -t nat -C OUTPUT -p tcp -s $src_addr -j SHADOWTABLES >& /dev/null \
+		&& iptables -t nat -D OUTPUT -p tcp -s $src_addr -j SHADOWTABLES
 }
 
 add_tables(){
 
-	sudo iptables -t nat -N SHADOWTABLES >& /dev/null
+	iptables -t nat -N SHADOWTABLES >& /dev/null
 
-	for ip in ${ignore_ips[@]} ;do
-		sudo iptables -t nat -C SHADOWTABLES -d $ip -j RETURN >& /dev/null \
-			|| sudo iptables -t nat -I SHADOWTABLES 1 -d $ip -j RETURN
+	#for ip in ${ignore_ips[@]} ;do
+	#	iptables -t nat -C SHADOWTABLES -s $src_addr -d $ip -j RETURN >& /dev/null \
+	#		|| iptables -t nat -I SHADOWTABLES 1 -s $src_addr -d $ip -j RETURN
+	#done
+
+	ipset create chnroute hash:net -exist
+	while IFS='' read -r ip || [[ -n "$ip" ]]; do
+    		ipset add chnroute $ip -exist
+	done < "$chnroute_file"
+	
+	for ip in ${ignore_ips[@]}; do
+		ipset add chnroute $ip -exist
 	done
 
-	sudo ipset create chnroute hash:net -exist
-	cat $chnroute_file | sudo xargs -I ip ipset add chnroute ip -exist
+	iptables -t nat -C SHADOWTABLES -s $src_addr -m set --match-set chnroute dst -j RETURN >& /dev/null \
+		|| iptables -t nat -A SHADOWTABLES -s $src_addr -m set --match-set chnroute dst -j RETURN
 
-	sudo iptables -t nat -C SHADOWTABLES -m set --match-set chnroute dst -j RETURN >& /dev/null \
-		|| sudo iptables -t nat -A SHADOWTABLES -m set --match-set chnroute dst -j RETURN
-
-	sudo iptables -t nat -C SHADOWTABLES -p tcp -j REDIRECT --to-ports $local_port >& /dev/null \
-		|| sudo iptables -t nat -A SHADOWTABLES -p tcp -j REDIRECT --to-ports $local_port
+	iptables -t nat -C SHADOWTABLES -p tcp -s $src_addr -j REDIRECT --to-ports $local_port >& /dev/null \
+		|| iptables -t nat -A SHADOWTABLES -p tcp -s $src_addr -j REDIRECT --to-ports $local_port
 
 }
 
@@ -68,7 +70,7 @@ check_for_update(){
 	  current=`date +%s`
 	  last_modified=`stat -c "%Y" $chnroute_file`
 	  if [[ $(($current-$last_modified)) -gt 604800 ]]; then
-	    if ping -q -c 1 -W 1 ftp.apnic.net >/dev/null; then
+	    if ping -q -c 1 -W 1 github.com >/dev/null; then
 	      update
 	    fi
 	  fi
@@ -79,7 +81,10 @@ check_for_update(){
 
 
 case $1 in
-
+	update_ip_list)
+		update
+		;;
+		
 	stop)
 		stop_rule
 		;;
